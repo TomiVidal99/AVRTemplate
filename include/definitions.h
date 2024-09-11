1ff7ddac6ff35424a186f8c66513896510c4a395
Tomás Vidal <tomividal9@gmail.com>
2024-09-10 23:24:09 -0300

    Anda en la simulacion y hace un control con lazo cerrado

--- include/definitions.h
+++ include/definitions.h
@@ -1,7 +1,6 @@
 #include <stdint.h>
 
-void init_adc5();   // hago la lectura de la muestra con el ADC5
-void init_ports();  // accion de control con el puerto PD2
-void init_timer1(); // este timer es el 16 bits, hace el PWM (a 1KHz)
-void init_timer0(); // este timer es de 8 bits, dispara las lecturas
-void update_pwm();  // ajusta el ciclo de trabajo del PWM y lo muestra en la salida (PD2)
+void init_adc5();
+void init_timer1();
+void read_adc5();
+void set_pwm_duty_cycle(uint8_t);
--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 /*
- * TP3_problema_2.c
+ * main.c
  *
  * Created: 14/7/2024 22:03:25
  * Author : Tomas Vidal
- * La precision de la lectura es horrible
+ * Control básico de planta
+ *
+ * Que hace?
+ * Con el Timer 1 (16 bits) se genera una interrupcion cada 1ms y un PWM de esa frecuencia
+ * el callback de la interrupcion ajusta el ciclo de trabajo del PWM (accion de control)
+ * a partir de leer el ADC5 y con calcular ajusta el PWM.
  */
 
 #include <stdio.h>
@@ -14,17 +19,22 @@
 
 #include "../include/definitions.h"
 
-uint16_t adc_read;
-uint8_t pwm_duty_cycle = 128;
+#define GAIN_FACTOR 10
+#define MIN_GAIN_FACTOR 80
+
+uint16_t system_output_mv;
 
 int main(void)
 {
-  init_ports();
   init_adc5();
   init_timer1();
-  init_timer0();
+  sei();
+
+  DDRD |= (1 << PD1);
 
   // set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Modo de bajo consumo: power-down
+
+  set_pwm_duty_cycle(50);
 
   while (1)
   {
@@ -35,50 +45,73 @@ int main(void)
 
 void init_timer1()
 {
-  TCCR1B |= (1 << WGM12);
-  OCR1A = 15625;
-  TIMSK1 |= (1 << OCIE1A);
-  TCCR1B |= (1 << CS12) | (1 << CS10);
+  // Set PB1 (OC1A) as output
+  DDRB |= (1 << PB1);
+
+  // Configure Timer 1 for Fast PWM mode with ICR1 as TOP
+  // WGM13:0 = 1110 for Fast PWM with ICR1 as top
+  TCCR1A = (1 << COM1A1) | (1 << WGM11);              // Clear OC1A on Compare Match, set OC1A at BOTTOM (non-inverting mode)
+  TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS10); // Fast PWM, no prescaler
+
+  // Set ICR1 for 1 kHz frequency (1 ms period)
+  ICR1 = 15999; // (F_CPU / (Prescaler * Frequency)) - 1 = 16000000 / (1 * 1000) - 1
+
+  // Enable Timer 1 overflow interrupt
+  TIMSK1 = (1 << TOIE1);
+
+  // Enable global interrupts
   sei();
+}
+
+void set_pwm_duty_cycle(uint8_t duty_cycle)
+{
+  // Limit duty cycle to 0-100%
+  if (duty_cycle > 100)
+  {
+    duty_cycle = 100;
+  }
+
+  // Calculate OCR1A value based on duty cycle percentage
+  OCR1A = (uint16_t)(((uint32_t)duty_cycle * (ICR1 + 1)) / 100);
+}
+
+// Timer1 Overflow ISR, called every 1 ms
+uint16_t control_action = 0;
+ISR(TIMER1_OVF_vect)
+{
+  // Code to run every 1 ms
+  // Example: Toggle an LED or perform some task
+
+  PORTD ^= (1 << PD1);
+
+  read_adc5();
+
+  control_action = GAIN_FACTOR * (5000 / (system_output_mv * 100)) + MIN_GAIN_FACTOR;
+  set_pwm_duty_cycle(control_action);
 }
 
 void init_adc5()
 {
-  ADMUX = (1 << REFS1) | (1 << REFS0) | (0 << ADLAR) | (0 << MUX3) | (1 << MUX2) | (0 << MUX1) | (1 << MUX0);
-  ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADEN);
+  // Select Vref = AVcc with external capacitor at AREF pin
+  ADMUX = (1 << REFS0);
+
+  // Select ADC5 (PC5) as input channel (MUX bits = 0101)
+  ADMUX |= (1 << MUX2) | (1 << MUX0);
+
+  // Enable ADC and set prescaler to 128 for 125 kHz ADC clock (16 MHz / 128)
+  ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+void read_adc5()
+{
+  // Start an ADC conversion by setting ADSC bit
   ADCSRA |= (1 << ADSC);
-  while ((ADCSRA & (1 << ADSC)) != 0)
-  {
-  }
-}
 
-ISR(TIMER1_COMPA_vect)
-{
-  ADCSRA |= (1 << ADSC);
-  while ((ADCSRA & (1 << ADSC)) != 0)
-  {
-  }
-  adc_read = ADC; // TODO: escalar correctamente
+  // Wait for the conversion to complete (ADSC becomes '0' again)
+  while (ADCSRA & (1 << ADSC))
+    ;
 
-  update_pwm();
-}
+  // Read the ADC value (10-bit result)
 
-void init_ports()
-{
-  DDRD |= (1 << PD2);
-}
-
-void init_timer0()
-{
-  TCCR0A |= (1 << WGM00) | (1 << WGM01);
-  TCCR0A |= (1 << COM0B1);
-  TCCR0B |= (1 << CS00);
-  OCR0B = pwm_duty_cycle;
-}
-
-void update_pwm()
-{
-  PORTD ^= (1 << PD2);
-  // pwm_duty_cycle = (uint8_t)((adc_read * 255) / 1023);
-  // OCR0B = pwm_duty_cycle;
+  system_output_mv = (uint16_t)((5000 * ADC) / 1023);
 }
